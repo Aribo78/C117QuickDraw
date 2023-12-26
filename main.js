37fe13f681a463e4c550ade303d417e5a5b76175
Aribo78 <89050884+Aribo78@users.noreply.github.com>
2023-12-26 14:33:05 -0500

    Update main.js

--- main.js
+++ main.js
@@ -8,3 +8,56 @@ timer_check = "";
 drawn_sketch =  "";
 answer_holder = "";
 score = 0;
+
+function preload()
+{
+classifier = ml5.imageClassifier('DoodleNet');
+}
+
+function classifyCanvas() {
+    classifier.classify(canvas, gotResult);
+}
+
+function setup()
+{
+    canvas = createCanvas(300,300);
+    canvas.center();
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
+
+}
+function draw()
+{
+    background("white");
+    strokeWeight(10);
+    stroke(0);
+    if (mouseIsPressed) {
+        line (pmouseX, pmouseY, mouseX, mouseY)
+    }
+}
+function updateCanvas()
+{
+    background("white");
+    random_number = Math.floor((Math.random()*quick_draw_set.length)+1);
+Element_of_array = quick_draw_data_set[random_number];
+document.getElementById("sketch_to_be_drawn").innerHTML = "Sketch To Be Drawn: "+Element_of_array;
+}
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    console.log(results);
+    document.getElementById('label').innerHTML = 'Label:' + results[0].label;
+
+    document.getElementById('confidence').innerHTML = 'Confidence' + Math.round(results[0].confidence * 100) + '%';
+
+    utterThis - new SpeechSynthesisUtterance(results[0].label);
+    synth.speak(utterThis);
+}
+
+check_sketch()
+{
+     
+}
+
